6480af2d7fe7ddebe1a6fb9b0ca5d89ab1db774b
gmorse19 <malorse@comcast.net>
2020-11-23 13:59:59 -0500

    Messing around

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button, 
+  Alert} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to Klever Boxing!</Text>
+      <Image 
+        style={styles.gloves}
+        source={require('./box-gloves.jpg')}
+      />
+      <Button
+        title='Instagram'
+        onPress={() => Alert.alert('You pressed the Button!!!')}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +32,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  gloves: {
+    width: '100%',
+    height:'40%'
+  },
 });
+
